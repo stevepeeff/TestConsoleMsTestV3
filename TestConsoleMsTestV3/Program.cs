d1fb8ccfa524012fe6f7e3880d42401656fd1a32
Stefan Matthijssen <s.matthijssen@gmail.com>
2024-08-28 10:57:16 +0200

    Package Setup

--- TestConsoleMsTestV3/Program.cs
+++ TestConsoleMsTestV3/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.Extensions.TrxLogger;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+using Microsoft.VisualStudio.TestPlatform.CrossPlatEngine;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using Microsoft.Testing.Platform.Extensions.TestFramework;
 
 namespace TestConsoleMsTestV3
 {
@@ -6,6 +16,10 @@ namespace TestConsoleMsTestV3
     {
         private static void Main(string[] args)
         {
+            ITestFramework x;
+
+            //  var tesplatform = new
+
             Console.WriteLine("Hello, World!");
         }
     }
---
+++ TestConsoleMsTestV3/TestExample.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleMsTestV3
+{
+    public class TestExample
+    {
+        [TestMethod]
+        public void TestSuccess()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
