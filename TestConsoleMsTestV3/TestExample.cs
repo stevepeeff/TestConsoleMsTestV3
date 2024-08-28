0a6b8e89bf866f363baffb57a9173873026bac86
Stefan Matthijssen <s.matthijssen@gmail.com>
2024-08-28 11:45:53 +0200

    Multi Target support for net48 and net8.0

--- TestConsoleMsTestV3/TestExample.cs
+++ TestConsoleMsTestV3/TestExample.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace TestConsoleMsTestV3
 {
+    /// <summary>
+    ///  Example of making an TestAssembly an Executable:
+    ///  https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-mstest-runner-intro
+    /// </summary>
     [TestClass]
     public class TestExample
     {
@@ -14,6 +18,12 @@ namespace TestConsoleMsTestV3
         public void TestSuccess()
         {
             Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestFailure()
+        {
+            Assert.Fail("This test must fail");
         }
     }
 }
